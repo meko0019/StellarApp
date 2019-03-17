7c4227e699bc56fef2915476c5b180aa7bd5f036
Aaron Mekonnen <meko0019@umn.edu>
2019-03-17 13:05:56 -0500

    initial setup

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'stellar.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
-
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
